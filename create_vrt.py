df7f2f6ff3b4adc7361c62abc520f6cf522c6887
Klas Karlsson <klaskarlsson@hotmail.com>
2016-01-30 15:20:54 +0100

    First update

---
+++ create_vrt.py
@@ -0,0 +1,32 @@
+"""
+Tool to create GDAL vrt files in ArcMap.
+Created for ArcGIS 10
+
+Takes 3 arguments
+1 - Folder path for input data (Mandatory)
+2 - Type of file, string with extension (Mandatory)
+3 - Output filename, inkluding path (Mandatory)
+
+Created by Klas Karlsson 2016-01-30
+"""
+
+
+
+import arcpy, subprocess
+from subprocess import call  
+
+# Get parameters from GUI
+InFolder = arcpy.GetParameterAsText(0)
+arcpy.AddMessage("Folder: " + InFolder)  
+InType = arcpy.GetParameterAsText(1)  
+arcpy.AddMessage("Type: " + InType)
+VrtFile = arcpy.GetParameterAsText(2)
+arcpy.AddMessage("Output: " + VrtFile)  
+
+# Create Call String
+CallString = "gdalbuildvrt " + VrtFile + " " + InFolder + "\\*." + InType  
+arcpy.AddMessage(CallString)
+
+# Execute the command
+process = subprocess.Popen(CallString, stdout=subprocess.PIPE, creationflags=0x08000000)
+process.wait()
